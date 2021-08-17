2a08595427adb0e75691541a4db422777c04f5ac
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-08-17 13:44:34 +1000

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+// Try and connect to the MySQL database
+$con = mysqli_connect('localhost','root','','glasses');
+// If connection was not successful, handle the error
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    // Handle error - log to a file, show an error screen
+}
