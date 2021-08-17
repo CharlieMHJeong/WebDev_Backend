91a52af6fefb66e6ca40fa5124a694ce794e69c9
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-08-17 15:07:11 +1000

    Update conn_count.php

--- conn_count.php
+++ conn_count.php
@@ -8,5 +8,15 @@ or die("Problem Reading table: " . mysqli_error($con));
 $intProductCount = mysqli_num_rows($dbProdRecords);
 echo "<p>We currently have $intProductCount products</p>";
 
+$sql_select = "SELECT * FROM products WHERE Name='Shot Glass'";
+$dbProdRecords = mysqli_query($con, $sql_select)
+or die("Problem Reading table: " . mysqli_error($con));
+
+if ($intProductCount > 0)
+    echo "<p> Yes we have a Shot glass</p>";
+else
+    echo "<p> Yes we have NO Shot glass</p>";
+
+
 mysqli_close($con);
 ?>
